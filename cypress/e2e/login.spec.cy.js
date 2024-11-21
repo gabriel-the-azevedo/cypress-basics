1e39311144e91b4039f066256ec4dbd874152c2c
Gabriel de Azevedo <184437223+gabriel-the-azevedo@users.noreply.github.com>
2024-11-21 11:10:29 -0300

    refactor: update project to use fixtures

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,16 +1,18 @@
+import userData from "../fixtures/user-data.json"
+
 describe('Orange HRM Demo', () => {
   it('should allow users to login if they use valid credentials', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get("[name='username']").type('Admin')
-    cy.get("[name='password']").type('admin123').type('{enter}')
+    cy.get("[name='username']").type(userData.validUserData.username)
+    cy.get("[name='password']").type(userData.validUserData.password).type('{enter}')
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
     cy.get('.oxd-topbar-header-breadcrumb').contains('Dashboard')
   })
 
   it('should not allow users to login with invalid credentials', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get("[name='username']").type('Admin')
-    cy.get("[name='password']").type('admin012').type('{enter}')
+    cy.get("[name='username']").type(userData.invalidUserData.username)
+    cy.get("[name='password']").type(userData.validUserData.password).type('{enter}')
     cy.get('[role=alert]')
   })
 })
