39d79486a0323d20415fcf226b06cdab3179c388
Gabriel de Azevedo <184437223+gabriel-the-azevedo@users.noreply.github.com>
2024-11-21 11:17:01 -0300

    refactor: add baseUrl to cypress.config

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -2,7 +2,7 @@ import userData from "../fixtures/user-data.json"
 
 describe('Orange HRM Demo', () => {
   it('should allow users to login if they use valid credentials', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get("[name='username']").type(userData.validUserData.username)
     cy.get("[name='password']").type(userData.validUserData.password).type('{enter}')
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
@@ -10,7 +10,7 @@ describe('Orange HRM Demo', () => {
   })
 
   it('should not allow users to login with invalid credentials', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get("[name='username']").type(userData.invalidUserData.username)
     cy.get("[name='password']").type(userData.validUserData.password).type('{enter}')
     cy.get('[role=alert]')
